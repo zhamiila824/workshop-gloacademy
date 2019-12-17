1c72e694fbc5c9f901bbd7db962cd82b8df51389
Zhamiila Kartanbaeva <jama.98.kjk@gmail.com>
2019-12-17 16:33:27 +0600

    1st day complete

---
+++ script/script.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const customer = document.getElementById('customer'),
+        freelancer = document.getElementById('freelancer'),
+        blockCustomer = document.getElementById('block-customer'),
+        blockFreelancer = document.getElementById('block-freelancer'),
+        blockChoice = document.getElementById('block-choice'),
+        btnExit = document.getElementById('btn-exit'),
+        formCustomer = document.getElementById('form-customer');
+
+    const orders = [];
+
+    customer.addEventListener('click', () => {
+        blockChoice.style.display = 'none';
+        blockCustomer.style.display = 'block';
+        btnExit.style.display = 'block';
+    });
+
+    freelancer.addEventListener('click', () => {
+        blockChoice.style.display = 'none';
+        blockFreelancer.style.display = 'block';        
+        btnExit.style.display = 'block';
+    });
+
+    btnExit.addEventListener('click', () => {
+        btnExit.style.display = 'none';
+        blockCustomer.style.display = 'none';
+        blockFreelancer.style.display = 'none';
+        blockChoice.style.display = 'block';
+    });
+
+    formCustomer.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const obj = {};
+        for (const elem of formCustomer.elements) {
+            if ((elem.tagName === 'INPUT' && elem.type !== 'radio') ||
+                (elem.type === 'radio' && elem.checked) ||
+                (elem.tagName === 'TEXTAREA')){
+                obj[elem.name] = elem.value;
+
+                if (elem.type !== 'radio'){
+                    elem.value = '';
+                }
+            }
+
+        }
+
+        orders.push(obj);
+    });
+
+});
